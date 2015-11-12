1236a04abc5470e10b54a91db157852fdf53ac11
sanjoy <sanjoy120@gmail.com>
2015-11-12 12:48:32 +0600

    remove npm from dependency

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@
 import Server from 'socket.io'
 
 export default function startServer(store){
-    const io = new Server().attach(8888);
+    const io = new Server().attach(8090);
     store.subscribe(()=>{
         io.emit('state', store.getState().toJS())
     });
